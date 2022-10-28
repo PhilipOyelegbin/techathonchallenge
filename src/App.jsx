de1bc13d1b9d65e9e978d6f7d08fcbb8b1d411d9
Philip Oyelegbin <philipoyelegbin@gmail.com>
2022-10-28 11:53:52 +0100

    boilerplate created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-fuchsia-600 text-center text-4xl font-bold'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
